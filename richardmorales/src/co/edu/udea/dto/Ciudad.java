13d13a5ba82740e53d6c6d7a750a58c740ae7665
estudiantelis <estudiantelis@lis-sala4>
2017-03-09 12:51:56 -0500

    v0.0

---
+++ richardmorales/src/co/edu/udea/dto/Ciudad.java
@@ -0,0 +1,33 @@
+package co.edu.udea.dto;
+
+public class Ciudad {
+	private long codigo;
+	private String nombre;
+	private String codigoArea;
+	public long getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getCodigoArea() {
+		return codigoArea;
+	}
+	
+	public void setCodigoArea(String codigoArea) {
+		this.codigoArea = codigoArea;
+	}
+	
+	
+
+}
